0bcdb99482fe1fbdf050537c369860f5d90c9454
Ricardo <rncliestpta@gmail.com>
2022-06-13 17:30:49 -0500

    sesion v5

--- Controllers/Session.php
+++ Controllers/Session.php
@@ -8,10 +8,21 @@ class Session extends Controller{
     }
     function render(){
         if (isset($_SESSION["log_User"]) and isset($_SESSION["log_Pass"])) {
+            $this->view->render('Dashboard/index');
+        }
+        else {
+            $this->view->render('Session/index');
+            //$this->view->render("Dashboard/index");
+        }
+        
+    }
+    function SessionConetion(){
+        if ($this->model->login(["log_User" => $_GET["User"],"log_Pass" => $_GET["Pass"]])) {
             
             $_SESSION["log_User"]=$_GET["User"]; 
             $_SESSION["log_Pass"]=$_GET["Pass"];
             $this->view->render('Dashboard/index');
+            ;
         }
         else {
 
@@ -19,8 +30,9 @@ class Session extends Controller{
         //$this->view->render("Dashboard/index");
         echo "ingreso sesion conection";
         }
-        
     }
+
+
     function SessionDestroy(){
         echo 'secion destruida';
         session_destroy();
--- Models/Sessionmodel.php
+++ Models/Sessionmodel.php
@@ -8,7 +8,7 @@ class SessionModel extends Model{
     public function login($datos){
         echo "modelo session";
         var_dump($datos);
-        $log_User=$datos["log_Use"];
+        $log_User=$datos["log_User"];
         $log_Pass=$datos["log_Pass"];
 
         echo "<br>".$log_User;
@@ -19,20 +19,15 @@ class SessionModel extends Model{
             FROM usuario
             WHERE usuario.log_User= :log_User and usuario.log_Pass= :log_Pass");
             $query->execute(["log_User"=>$log_User,"log_Pass"=>$log_Pass]);
-            //$query->bindParam(":log_User",);
-            //$query->bindParam(":log_Pass",);
-            //$stmt->execute();
-            //var_dump($stmt);
             while ($row=$query->fetch()) {
-                //var_dump($query);
                 $item=new V_Session();
                 $item->log_User=$row['log_User'];
                 $item->log_Pass=$row['log_Pass'];
-                //echo $row['log_User'];
+                
                 array_push($items,$item);
 
             }
-            //var_dump($items);
+            
             return $items;
        } catch (PDOException $e){
             echo "falla de salida";
