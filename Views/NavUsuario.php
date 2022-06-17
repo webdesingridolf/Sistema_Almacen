1d91a3f6d6c79442f3f371f0d2e1754668b0f471
Ricardo <rncliestpta@gmail.com>
2022-06-16 23:16:57 -0500

    papitas3.4

--- Views/NavUsuario.php
+++ Views/NavUsuario.php
@@ -10,7 +10,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="<?php echo constant('BASE_URL');?>Dashboard" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -141,7 +141,7 @@
    </nav>
         <aside class="main-sidebar sidebar-dark-primary elevation-4 nav-vertical ">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="<?php echo constant('BASE_URL');?>Dashboard" class="brand-link">
       <img src="<?php echo constant('BASE_URL');?>assets/img/Logo_GRTPE.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
